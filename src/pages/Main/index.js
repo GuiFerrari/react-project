1e8b9785b5f3dca7a608be95674dd1d86d0efa44
GuiFerrari <guiferrari10@hotmail.com>
2020-03-24 01:51:33 -0300

    Created styled of Main page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-// import { Container } from './styles';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-    return <h1>Main</h1>;
+    const [repo, setRepo] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(repo);
+    }
+
+    return (
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositórios
+            </h1>
+            <Form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Adicionar repositório"
+                    value={repo}
+                    onChange={e => setRepo(e.target.value)}
+                />
+                <SubmitButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 }
